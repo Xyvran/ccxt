92fea510b47ef9a08d870e861fc58741eea04cc1
Jarvis <jarvis@nwan.de>
2026-06-14 13:31:11 +0000

    Fix: use withResponseBuffer() instead of withOptions() for 32 MiB limit
    withOptions() is private in React\Http\Browser. Use the public
    withResponseBuffer() API instead to raise the HTTP response body limit.

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -75,7 +75,7 @@ class Exchange extends \ccxt\Exchange {
     }
 
     public function set_request_browser($connector) {
-        $this->browser = (new React\Http\Browser($connector, Loop::get()))->withRejectErrorResponse(false)->withOptions(['maximumSize' => 33554432]);
+        $this->browser = (new React\Http\Browser($connector, Loop::get()))->withRejectErrorResponse(false)->withResponseBuffer(33554432);
     }
 
     public function create_connector ($connector_options = array()){
